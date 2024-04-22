777949fc38bfdb21389df6603f740bff40eada8b
Gamadiel <gamadiel-hm@hotmail.com>
2024-04-21 22:04:38 -0600

    Add project files.

---
+++ Controllers/Notification.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalIrNotifications.Hubs;
+
+namespace SignalIrNotifications.Controllers
+{
+    [ApiController]
+    [Route("api/v1/notifiaction")]
+    public class Notification(IHubContext<NotificationHub> hubContext) : ControllerBase
+    {
+        private readonly IHubContext<NotificationHub> _hubContext = hubContext;
+
+        [HttpPost]
+        public async Task<IActionResult> Send(string message)
+        {
+            await _hubContext.Clients.All.SendAsync("sendMessage",message);
+
+            return Ok( );
+        }
+
+        [HttpPost("newnotification")] 
+        public async Task<IActionResult> NewNotification(string room, string message)
+        {
+            await _hubContext.Clients.Group(room).SendAsync("messageGroup", message);
+
+            return Ok();
+        }
+    }
+}
---
+++ Hubs/NotificationHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalIrNotifications.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task AddGroup(string room,string usuario)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+
+            await Clients.Group(room).SendAsync("joinGroup", $"Join chat: {usuario}");
+        }
+    }
+}
